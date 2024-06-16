0c2f67e7e7940a2508a747f81f8dcb9382f1d24f
Bimlesh kumar <bimleshgps@gmail.com>
2024-06-17 02:10:06 +0530

    implementation of bcrypt in routes folder

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userRouter = express.Router();
 
@@ -8,10 +9,26 @@ userRouter.post("/register", async (req, res) => {
   //   res.send("Registeration Page");
 
   try {
-    const userData = req.body;
-    const userModel = await new UserModel(userData);
-    userModel.save();
-    res.status(200).send({ msg: " New User has been registered" });
+    const { username, email, password, age, role } = req.body;
+    bcrypt.hash(password, 5, async (error, hash) => {
+      if (error) {
+        res
+          .status(404)
+          .send(
+            `something went wrong in creating the hash password with error ${error}`
+          );
+      } else {
+        const userModel = new UserModel({
+          username,
+          email,
+          password: hash,
+          age,
+          role,
+        });
+        await userModel.save();
+        res.status(200).send({ msg: " New User has been registered" });
+      }
+    });
   } catch (error) {
     res
       .status(404)
@@ -24,12 +41,30 @@ userRouter.post("/login", async (req, res) => {
     const { email, password } = req.body;
     // const filter = req.body;
     // console.log(filter);
-    const user = await UserModel.findOne({ email, password });
-    const token = jwt.sign({ email: user.email, role: user.role }, "self");
+    const user = await UserModel.findOne({ email });
     if (user) {
-      res.status(200).send({ msg: " Login Successfull", token: token });
+      bcrypt.compare(password, user.password, (error, result) => {
+        //result --> boolean (true or false)
+        if (error) {
+          res
+            .status(404)
+            .send(
+              `something went wrong in comapring the password with error ${error}`
+            );
+        }
+
+        if (result) {
+          const token = jwt.sign(
+            { email: user.email, password: user.passoword, role: user.role },
+            "self"
+          );
+          res.status(200).send({ msg: " Login Successfull", token: token });
+        } else {
+          res.status(200).send({ msg: "wrong Credentials try again!!!" });
+        }
+      });
     } else {
-      res.status(200).send({ msg: " Wrong Credentials" });
+      res.status(200).send({ msg: " Wrong email Credentials" });
     }
   } catch (error) {
     res
